362480e547b3d11deb22ead05d335188b138d706
Yunior Miguel Laureano <yu.laureano@dga.gov.do>
2019-03-19 14:39:42 -0400

    Webpack with ract from 0

---
+++ src/containers/Users.js
@@ -0,0 +1,16 @@
+import { React, Component } from "react";
+import PizzaImage from '../components/PizzaImage/PizzaImage';
+
+
+class Users extends Component {
+    render(){
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>Awesome suers on board of this course!</p>
+            </div>
+        );
+    }
+}
+
+export default Pizza;
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: './src/index.js',
+    output:{
+        path:'',
+        filename: 'bouldle.js',
+        publicPath: path.resolve(__dirname, 'dist')
+    },
+    resolve:{
+        extensions: ['.js', '.jsx'] 
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options:{
+                            importLoaders: 1,
+                            modules: true,
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                        }
+                    }
+                ]
+            },
+            {
+                loader:'postcss-loader',
+                options:{
+                    ident: 'postcss',
+                    plugins:()=>[
+                        autoprefixer({
+                            browsers:[
+                                ">1%",
+                                "last 2 versions"
+                            ]
+                        })
+                    ]
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?'
+            }
+        ]
+    }
+};
